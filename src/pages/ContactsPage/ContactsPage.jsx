051b39f1024d76945424f246594963229bc213e1
AndreyGOIT <ae.borga@gmail.com>
2023-02-01 18:19:30 +0200

    Update ContactsPage.jsx

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -6,6 +6,7 @@ import { AddFormModal } from '../../components/AddFormModal/AddFormModal';
 export const ContactsPage = () => {
   return (
     <Container maxW="container.lg" py={6}>
+      {/* // <Container size="full" py={6}> */}
       <VStack spacing="5">
         <Box>
           <Flex justify="center" align="center" gap="10px">
